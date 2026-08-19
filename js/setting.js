1d5c3b305ac734c5572d7fd00f352e287296ddbb
yasmin samak <yasminsamak6@gmail.com>
2026-08-19 15:31:36 +0300

    setting add new parts +solve some bugs in all code

--- js/setting.js
+++ js/setting.js
@@ -1,1 +1,23 @@
 // ------------------ Notification ----------------
+//------------------- appearance -----------------
+document.addEventListener("DOMContentLoaded", function () {
+  const themeBtns = document.querySelectorAll(".theme-btn");
+
+  themeBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+
+      const selectedBtn = e.target.closest(".theme-btn");
+      if (!selectedBtn) return;
+
+      themeBtns.forEach((b) => b.classList.remove("active"));
+      selectedBtn.classList.add("active");
+
+
+      const selectedTheme = selectedBtn.getAttribute("data-theme");
+
+
+      document.body.classList.remove("theme-cyan", "theme-purple", "theme-lime");
+      document.body.classList.add(`theme-${selectedTheme}`);
+    });
+  });
+});
