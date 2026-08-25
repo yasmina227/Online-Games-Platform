98f13d9c477e557c47144c34bcf6bbb961977ce3
yasmin samak <yasminsamak6@gmail.com>
2026-08-25 22:07:03 +0300

    solve style bugs in login &signup pages and edit profile to get data from local storage

---
+++ js/profile.js
@@ -0,0 +1,16 @@
+const username= document.querySelector(".user-name");
+const usertag= document.querySelector(".user-tag");
+const rankbage= document.querySelector(".rank-badge");
+const email= document.querySelector("#email");
+const joineddate= document.querySelector("#joineddate");
+
+
+ let user = JSON.parse(localStorage.getItem("recentUser"));
+
+username.innerHTML=user.username;
+usertag.innerHTML="@"+user.username;
+rankbage.innerHTML=user.profile.rank;
+email.innerHTML=user.email;
+joineddate.innerHTML=user.createdAt;
+
+
